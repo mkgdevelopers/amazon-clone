dff818eeb77471fd44d18c09707c8c893a7ee4f2
mkgdevelopers <muaaznaeemoff@gmail.com>
2025-04-13 20:27:10 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// console is an object and it has many methods like log,warn error,table
+
+
+// console.log("mkbg")
+
+// let name ="gori"
+
+// console.log(name)
+
+
+
+// primitive data type 
+/*number 
+console.log(2+2)
+Console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2%2) 
+console.log(2/2)
+
+
+     counter++; (for 1+)
+     counter--; (for one -)
+*/
+
+
+// - booleans
+// false
+// 0
+// -0
+// -null 
+// undefined
+// NaN
+
+// appand  ( to jointed anything+ )
+// concatination
+
+// let n="shoaib"
+// let p="phudu"
+
+
+// console.log(n.concat(p))
+
+// let n='4', t=2
+
+// console.log(n!=t)
+
+// let username=prompt("enter your name : ");
+
+// console.log(username)
+
+
+// let city="pak";
+
+// console.log(city)
+
+
+// let isStudent=true
+// console.log(isStudent)
